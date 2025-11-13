a28cd64b70646b72a6a4c1512eb9460405051696
Emiliano Daniel Guerrero <guerrero.emiliano@outlook.com>
2025-11-13 12:10:12 -0300

    Init maven spring boot

---
+++ src/main/java/com/bank_services/account/BankAccountServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bank_services.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountServiceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank_services/account/BankAccountServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank_services.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
